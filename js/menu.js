bb570c020dec97a1ecc4e2b1428ddd01489a86f7
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-10 20:03:05 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,9 +5,7 @@ $(document).ready(function() {
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/windows">Microsoft Windows</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows/post/cmd/network.html">Εντολές Δικτύου</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-     menuLink = '<li><a href="https://grcodeclub.github.io/windows/post/cmd/system.html">Εντολές Συστήματος</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io/windows/command">Γραμμή εντολών</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/windows/post/language_id.html">Language ID</a></li>';
     $('.sidebarMenuInner').append(menuLink);
