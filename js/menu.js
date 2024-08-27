08363d586b4fcc5bd57d20dd21042e8a6e90cec8
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-27 13:34:28 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-    var menuLink = ' <li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows">Microsoft Windows</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows/orologia">Ορολογία</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows/command">Γραμμή εντολών</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows/command/powershell">PowerShell</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows/more/run">Run Commands</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows/more/language-id">Language ID</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li style="height: 200px;"></li>';
-    $('.sidebarMenuInner').append(menuLink);
-  });
-  
+window.onload = function() {
+    var menuLinks = [
+        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows">Microsoft Windows</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows/orologia">Ορολογία</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows/command">Γραμμή εντολών</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows/command/powershell">PowerShell</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows/more/run">Run Commands</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows/more/language-id">Language ID</a></li>',
+        '<li style="height: 200px;"></li>';
+    ];
+    
+    var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
+    $sidebarMenuInners.forEach(function($sidebarMenuInner) {
+    menuLinks.forEach(function(link) {
+        $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+    });
+});
+};
